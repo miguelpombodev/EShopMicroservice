42f649daa5f9534731c4c06cc11a92f4fa69055b
Miguel Pombo <miguel.pomboshape@modec.com>
2025-03-19 18:22:16 -0300

    feat: add BuildingBlocks to unite all common packages from microservices and split abstraction from CQRS

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,20 @@
+using MediatR;
+
 namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductHandler
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageUrl,
+    decimal Price) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler:  IRequestHandler<CreateProductCommand, CreateProductResult>
 {
-    
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
